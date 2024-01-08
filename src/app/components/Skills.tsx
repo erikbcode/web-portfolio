b75c17e67aaef7f83a334b9bcc4cc047114ec788
Erik Buinevicius <erikbcode@gmail.com>
2024-01-08 00:11:25 -0600

    Added Skills section display

---
+++ src/app/components/Skills.tsx
@@ -0,0 +1,28 @@
+import SkillsBar from './SkillsBar'
+
+const Skills = () => {
+  return (
+    <section className='relative w-3/4 min-h-[50vh] flex flex-col justify-center items-center text-center gap-12 my-12'>
+      <h2 className='text-4xl font-semibold select-none cursor-default hover:scale-110 transition-all duration-500'>
+        Skills
+      </h2>
+      <SkillsBar />
+      <p className='text-sm md:text-lg font-lightlight select-none cursor-default'>
+        I possess a versatile skill set, showcasing proficiency in technologies
+        such as Typescript, React, and Next.js for robust front-end development,
+        complemented by expertise in Java and Python for powerful back-end
+        solutions. My experience extends to cloud computing, particularly with
+        AWS services, demonstrating a comprehensive understanding of scalable
+        and reliable cloud solutions.
+      </p>
+      <p className='text-sm md:text-lg font-lightlight select-none cursor-default'>
+        Beyond my current skill set, I pride myself on being an adaptable
+        learner, quick to grasp new tools and technologies. This agility allows
+        me to stay at the forefront of advancements, ensuring a dynamic and
+        innovative approach to problem-solving.
+      </p>
+    </section>
+  )
+}
+
+export default Skills
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,13 @@
-import Image from 'next/image'
-import Intro from './components/Intro'
 import About from './components/About'
+import Intro from './components/Intro'
+import Skills from './components/Skills'
 
 export default function Home() {
   return (
     <main className='w-full h-full z-20 grid grid-cols-1 place-items-center'>
       <Intro />
       <About />
+      <Skills />
     </main>
   )
 }
