1cdb40ec5cacc0844b5a3fca996b8374f6ff4175
Erik Buinevicius <erikbcode@gmail.com>
2024-01-08 00:11:38 -0600

    Improved styling

--- src/app/components/About.tsx
+++ src/app/components/About.tsx
@@ -1,22 +1,24 @@
-import React from 'react'
-
 const About = () => {
   return (
-    <section className='relative w-3/4 min-h-[50vh] flex flex-col items-center text-center mt-10 gap-8'>
-      <h2 className='text-4xl font-semibold'>About Me</h2>
-      <p className='text-sm md:text-lg font-light'>
+    <section
+      id='about'
+      className='relative w-3/4 min-h-[50vh] flex flex-col justify-center items-center text-center gap-12 my-12'
+    >
+      <h2 className='text-4xl font-semibold select-none cursor-default hover:scale-110 transition-all duration-500'>
+        About Me
+      </h2>
+      <p className='text-sm md:text-lg font-light select-none cursor-default'>
         Full-stack engineer with a sharp focus on building efficient and
         impactful solutions. Passionate about crafting seamless user
-        experiences, while implementing robust backend functionality. Experience
-        in TypeScript, React, Next.js, Java, Python, and more.
+        experiences, while implementing robust backend functionality.
       </p>
-      <p className='text-sm md:text-lg font-light'>
+      <p className='text-sm md:text-lg font-light select-none cursor-default'>
         My experience working as a developer on a wide range of teams, from
         fintech & asset management all the way to services supporting B2B
         purchasing, gives me a unique perspective and skillset when approaching
         any problem.
       </p>
-      <p className='text-sm md:text-lg font-light'>
+      <p className='text-sm md:text-lg font-light select-none cursor-default'>
         Beyond the code, I'm eager to collaborate and learn from experienced
         developers. I'm a passionate believer in continuous growth, constantly
         seeking new ways to expand my knowledge and skillset.
