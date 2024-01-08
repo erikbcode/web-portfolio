bdcf892c6116918438c3eefeee80865bc6b9d96b
ERIK BUINEVICIUS <erikbcode@gmail.com>
2024-01-07 21:09:18 -0600

    Add fade-in effect

--- src/app/components/StarsBackground.tsx
+++ src/app/components/StarsBackground.tsx
@@ -2,13 +2,23 @@
 import { PointMaterial, Points } from '@react-three/drei'
 import { Canvas, useFrame } from '@react-three/fiber'
 import { inSphere } from 'maath/random'
-import { useRef, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 
 const StarsBackground = () => {
   const ref: any = useRef()
   const [sphere] = useState(() =>
     inSphere(new Float32Array(5000), { radius: 1.5 })
   )
+  const [starOpacity, setStarOpacity] = useState(0)
+
+  useEffect(() => {
+    const fadeStarsIn = () => {
+      setStarOpacity((opacity) => Math.min(opacity + 0.01, 1))
+    }
+    const interval = setInterval(fadeStarsIn, 10) // Adjust interval for fade speed
+    return () => clearInterval(interval) // Clear interval on unmount
+  })
+
   useFrame((state, delta) => {
     ref.current.rotation.x -= delta / 20
     ref.current.rotation.y -= delta / 30
@@ -22,6 +32,7 @@ const StarsBackground = () => {
           size={0.005}
           sizeAttenuation={true}
           depthWrite={false}
+          opacity={starOpacity}
         />
       </Points>
     </group>
