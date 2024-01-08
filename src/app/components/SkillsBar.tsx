c18107bb4ba8578981a4069878e1e51979c7a71b
Erik Buinevicius <erikbcode@gmail.com>
2024-01-08 00:11:17 -0600

    Added infinite scrolling skill icon bar

---
+++ src/app/components/SkillsBar.tsx
@@ -0,0 +1,55 @@
+import Image from 'next/image'
+
+const skills = [
+  'typescript',
+  'react',
+  'next',
+  'tailwind',
+  'prisma',
+  'java',
+  'python',
+  'aws',
+]
+
+const SkillsBar = () => {
+  return (
+    <div className='relative w-full h-20 inline-flex flex-nowrap overflow-hidden'>
+      <div
+        className={`absolute w-full h-full flex items-center animate-infinite-scroll`}
+      >
+        {skills.map((skill, index) => {
+          return (
+            <Image
+              key={index}
+              src={`/images/${skill}-logo.png`}
+              alt={skill}
+              width={0}
+              height={0}
+              sizes='100vw'
+              className='w-auto h-20 mx-auto'
+            />
+          )
+        })}
+      </div>
+      <div
+        className={`absolute w-full flex items-center left-[100%] animate-infinite-scroll`}
+      >
+        {skills.map((skill, index) => {
+          return (
+            <Image
+              key={index}
+              src={`/images/${skill}-logo.png`}
+              alt={skill}
+              width={0}
+              height={0}
+              sizes='100vw'
+              className='w-auto h-20 mx-auto'
+            />
+          )
+        })}
+      </div>
+    </div>
+  )
+}
+
+export default SkillsBar
