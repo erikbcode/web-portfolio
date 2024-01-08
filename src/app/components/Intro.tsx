4c60eecb7c42e76439c5effeaf34093c218a3df7
ERIK BUINEVICIUS <erikbcode@gmail.com>
2024-01-08 17:02:34 -0600

    Changed branding color and intro description

--- src/app/components/Intro.tsx
+++ src/app/components/Intro.tsx
@@ -14,13 +14,13 @@ const Intro = () => {
   return (
     <div className='relative min-h-screen flex flex-col justify-center items-center gap-8 text-center mx-4'>
       <Link href='/'>
-        <h1 className='text-4xl sm:text-7xl lg:text-9xl font-semibold hover:scale-110 text-white bg-gradient-to-tr from-pink-500 to-yellow-200 hover:text-transparent transition-all duration-300 bg-clip-text'>
+        <h1 className='text-4xl sm:text-7xl lg:text-9xl font-semibold hover:scale-110 text-white bg-gradient-to-tr from-blue-500 to-pink-500 hover:text-transparent transition-all duration-300 bg-clip-text'>
           Erik Buinevicius
         </h1>
       </Link>
 
       <h3 className='text-sm sm:text-lg select-none cursor-default'>
-        Full stack software engineer with experience in Java, React, Next.js
+        Full stack software engineer.
       </h3>
       <div className='flex flex-row gap-8'>
         <Link
