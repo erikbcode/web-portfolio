1c630667d1c1f50da76a00916466c71ab5b72666
ERIK BUINEVICIUS <erikbcode@gmail.com>
2024-01-22 00:09:15 -0600

    Added Education section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,17 @@
+import { Contact } from 'lucide-react'
 import About from './components/About'
 import Experience from './components/Experience'
 import Intro from './components/Intro'
 import Skills from './components/Skills'
+import FloatingWordsCanvas from './components/Three/FloatingWords'
+import Education from './components/Education'
 
 export default function Home() {
   return (
     <main className='w-full h-full z-20 grid grid-cols-1 place-items-center'>
       <Intro />
       <About />
+      <Education />
       <Skills />
       <Experience />
     </main>
