43b5f43a588950007a9e2db9c02379e49bda1685
ERIK BUINEVICIUS <erikbcode@gmail.com>
2024-01-22 13:33:27 -0600

    Initialized Contact page

---
+++ src/app/components/Contact.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Contact = () => {
+  return (
+    <div>
+      <h2 className='relative w-3/4 min-h-[50vh] flex flex-col justify-center items-center text-center gap-12 my-12'>
+        Contact
+      </h2>
+    </div>
+  )
+}
+
+export default Contact
